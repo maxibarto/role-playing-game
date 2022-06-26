714cf2fef0694b83656031da3c98083b88b8e2a2
Maxi Bartolozzi <maxibartolozzi@gmail.com>
2022-06-26 19:16:58 -0300

    initial commit - role-playing game part-3

---
+++ data.js
@@ -0,0 +1,32 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "images/wizard.jpeg",
+        health: 60,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Orc",
+        avatar: "images/orc.jpeg",
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.jpeg",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.jpeg",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+export default characterData
---
+++ readme.md
@@ -0,0 +1,19 @@
+<!-- Part 1 -->
+# Object destructuring
+# The .map() method
+# The .join() method
+# Return a function inside a function
+# Creating new arrays with the Array constructor
+# The .fill() method
+
+<!-- Part 2 -->
+# Constructor functions
+# The "this" keyword
+# Methods on constructor functions
+# Object.asign
+# Import / Export
+
+<!-- Part 3 -->
+# The .reduce() method
+# Arrow functions
+# The .setTimeOut() method
